8817f026ecced468f17a0e83e9137729a11af91e
Janda <SJanda@logility.com>
2023-01-30 22:02:41 -0500

    release candidate

--- StudentDatabase/Program.cs
+++ StudentDatabase/Program.cs
@@ -1,15 +1,15 @@
 ﻿
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-
 string[] name = { "Adam", "Barb", "Chris" };
-string[] hometown = { "Atlanta","Boston", "Cleveland"};
-string[] favoriteFood = { "Avacado Toast", "Buritos","Clam Chowder"};
-//int maxNameLength = name.OrderByDescending(s => s.Length).First().Length;
-
+string[] hometown = { "Atlanta", "Boston", "Cleveland" };
+string[] favoriteFood = { "Avacado Toast", "Buritos", "Clam Chowder" };
+int maxNameLength = name.OrderByDescending(s => s.Length).First().Length;
 string userContinue = "y";
+
 do
 {
+    bool searchLoopCheck = false;
+    bool detailsLoopCheck = false;
+
     Console.Write($"To learn about a student, please Enter a number 1 - {name.Length} or enter a student name to search by name. You may also enter \"list\" to see a list of all students:");
     string inputCheck = Console.ReadLine();
 
@@ -29,27 +29,22 @@ do
 
     if (validNumber == false)
     {
-
-
         int counter = 0;
         do
         {
-
-
-        foreach (var item in name)
-        {
-            //if (inputCheck.Trim().ToLower().Contains(item.ToLower()))
-            if (inputCheck.Trim().ToLower().Contains(item.ToLower().Remove(item.Length - counter))) //doest work with single character string
+            foreach (var item in name)
             {
-                userInput = Array.IndexOf(name, item) + 1;
+                //if (inputCheck.Trim().ToLower().Contains(item.ToLower()))
+                if (inputCheck.Trim().ToLower().Contains(item.ToLower().Remove(item.Length - counter)))
+                {
+                    userInput = Array.IndexOf(name, item) + 1;
+                    searchLoopCheck = true;
+                }
             }
-        }
 
-        counter += 1;
+            counter += 1;
 
-        } while (counter <= inputCheck.Length);
-
-
+        } while (counter < maxNameLength && searchLoopCheck == false); //counter <= inputCheck.Length);
     }
 
     if (userInput > name.Length || userInput < 1)
@@ -59,33 +54,36 @@ do
         continue;
     }
 
-        bool loopContinue = false;
-        do
+    do
+    {
+        Console.WriteLine($"Student {userInput} is {name[userInput - 1]}. What would you like to know? Enter hometown or favorite food:\n");
+        string userInput2 = Console.ReadLine();
+
+        if (userInput2.Trim().ToLower().StartsWith("hometown"))
         {
-            Console.WriteLine($"Student {userInput} is {name[userInput - 1]}. What would you like to know? Enter hometown or favorite food:");
-            string userInput2 = Console.ReadLine();
-            
-            if (userInput2.Trim().ToLower().StartsWith("hometown"))
-            {
-                Console.WriteLine($"{name[userInput - 1]} is from {hometown[userInput - 1]}");
-            }
-            else if (userInput2.Trim().ToLower().StartsWith("favorite"))
-            {
-                Console.WriteLine($"{name[userInput - 1]} likes to eat {favoriteFood[userInput - 1]}");
-            }
-            else
-            {
-                Console.WriteLine("That is not a valid category.");
-                Console.WriteLine();
-                loopContinue = true;
-            }
+            Console.WriteLine($"\n{name[userInput - 1]} is from {hometown[userInput - 1]}\n");
+            detailsLoopCheck = false;
+        }
 
-        } while (loopContinue);
+        else if (userInput2.Trim().ToLower().StartsWith("favorite"))
+        {
+            Console.WriteLine($"\n{name[userInput - 1]} likes to eat {favoriteFood[userInput - 1]}\n");
+            detailsLoopCheck = false;
+        }
 
-        Console.WriteLine("Would you like to learn about another student? Enter \"y\" or \"n\":");
-        userContinue = Console.ReadLine();
-        Console.Clear();
-    
-    } while (userContinue.ToLower() == "y");
+        else
+        {
+            Console.WriteLine("That is not a valid category.");
+            Console.WriteLine();
+            detailsLoopCheck = true;
+        }
+
+    } while (detailsLoopCheck);
+
+    Console.WriteLine("Would you like to learn about another student? Enter \"y\" or \"n\":");
+    userContinue = Console.ReadLine();
+    Console.Clear();
+
+} while (userContinue.ToLower() == "y");
 
 Console.WriteLine("Thanks!");
